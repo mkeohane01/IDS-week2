56fb44754e1863745f41fc696ef075e469b7a6fd
Mike Keohane <78706919+mkeohane01@users.noreply.github.com>
2023-09-10 16:40:41 +0000

    Was able to load and describe a baseball dataset from github

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv("https://github.com/datacamp/courses-introduction-to-python/blob/master/datasets/baseball.csv")
-df.describe()
+df = pd.read_csv("https://raw.githubusercontent.com/datacamp/courses-introduction-to-python/master/datasets/baseball.csv")
+description = df.describe()
+
+print(df.head(20))
+print(description)
