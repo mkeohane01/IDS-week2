2c183894d791c986ca6ff0ed8897a391d5c76e0b
Mike Keohane <78706919+mkeohane01@users.noreply.github.com>
2023-09-11 19:29:44 +0000

    Updated main.py to include visualizing the data through grouping by position

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
+
+# read and describe data
 df = pd.read_csv("https://raw.githubusercontent.com/datacamp/courses-introduction-to-python/master/datasets/baseball.csv")
 description = df.describe()
-
-print(df.head(20))
+print(df.head(10))
 print(description)
+
+position_counts = df['Position'].value_counts()
+
+# visualize and plot data
+plt.figure(figsize=(10, 6))
+position_counts.plot(kind='bar')
+plt.title('Histogram of Players by Position')
+plt.xlabel('Position')
+plt.ylabel('Number of Players')
+plt.xticks(rotation=45)  
+plt.tight_layout()
+plt.savefig('position_histogram.png')
+plt.show()
